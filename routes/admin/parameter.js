b7c3397c34e83701bdcfe1b24c713b577c50c559
balyaGround <balyazulfikar@gmail.com>
2022-03-09 13:29:13 +0700

    coba ngepush lagi

--- routes/admin/parameter.js
+++ routes/admin/parameter.js
@@ -6,7 +6,7 @@ const { admin } = require("../../middlewares/auth");
 const { getParameter, updateParameterCtrl } = require("../../controller/parameterController");
 
 const { logCtrl } = require("../../controller/logsController");
-//ayooo ini apa yaaa
+
 const { updateParameterVldt, updateTitleVldt, updateScheduleAttrVldt } = require("../../middlewares/validator/parameterValidator");
 
 route.get("/", getParameter);
--- routes/supervisor/parameter.js
+++ routes/supervisor/parameter.js
@@ -3,28 +3,15 @@ const route = express.Router();
 
 const { supervisor } = require("../../middlewares/auth");
 
-const {
-  getParameter,
-  updateParameterCtrl,
-} = require("../../controller/parameterController");
+const { getParameter, updateParameterCtrl } = require("../../controller/parameterController");
 
 const { logCtrl } = require("../../controller/logsController");
 
-const {
-  updateParameterVldt,
-  updateTitleVldt,
-  updateScheduleAttrVldt,
-} = require("../../middlewares/validator/parameterValidator");
+const { updateParameterVldt, updateTitleVldt, updateScheduleAttrVldt } = require("../../middlewares/validator/parameterValidator");
 
 route.get("/", getParameter);
 route.post("/", supervisor, updateParameterVldt, updateParameterCtrl, logCtrl);
 route.post("/title", supervisor, updateTitleVldt, updateParameterCtrl, logCtrl);
-route.post(
-  "/attributes",
-  supervisor,
-  updateScheduleAttrVldt,
-  updateParameterCtrl,
-  logCtrl
-);
+route.post("/attributes", supervisor, updateScheduleAttrVldt, updateParameterCtrl, logCtrl);
 
 module.exports = route;
