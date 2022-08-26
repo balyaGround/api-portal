9eae1d5139bbf6033c96f90013b491b3dd879c25
gilang.sukma <gilang.sukma@privyid.tech>
2022-08-26 17:07:02 +0700

    new func

--- controller/curlController.js
+++ controller/curlController.js
@@ -1,39 +1,61 @@
-var request = require("request");
-
+const http2 = require("http2");
 const fs = require("fs");
 const path = require("path");
-
-certFile = path.resolve(__dirname, "../cert/VOIP.pem");
 
 class curlController {
   notificationCtrl = async (req, res, next) => {
     try {
-      var headers = {
+      const host = "https://api.sandbox.push.apple.com";
+      const path =
+        "/3/device/b7da7ae53440e3dc7ce7f1cfeef18e345b612487469bbc5d4ab469c16b64e51b";
+
+      const client = http2.connect(host, {
+        key: fs.readFileSync(__dirname + "/cert/VOIP.pem"),
+        cert: fs.readFileSync(__dirname + "/cert/VOIP.crt.pem"),
+      });
+
+      client.on("error", (err) => console.error(err));
+
+      const body = {
+        // aps: {
+        //   alert: "hello",
+        //   "content-available": 1,
+        // },
+        aps: { alert: "UWAW KEREN KALI" },
+        id: "44d915e1-5ff4-4bed-bf13-c423048ec97a",
+        nameCaller: "UWAW",
+        handle: "0123456789",
+        isVideo: true,
+      };
+
+      const headers = {
+        ":method": "POST",
         "apns-topic": "id.ist.flutterWebrtcDemo.voip",
         "apns-push-type": "voip",
+        ":scheme": "https",
+        ":path": path,
       };
 
-      var options = {
-        url: "https://api.development.push.apple.com/3/device/b7da7ae53440e3dc7ce7f1cfeef18e345b612487469bbc5d4ab469c16b64e51b",
-        method: "POST",
-        headers: headers,
-        json: {
-          aps: { alert: "UWAW KEREN KALI" },
-          id: "44d915e1-5ff4-4bed-bf13-c423048ec97a",
-          nameCaller: "UWAW",
-          handle: "0123456789",
-          isVideo: true,
-        },
-        cert: fs.readFileSync(certFile),
-        passphrase: "ist2022",
-      };
+      const request = client.request(headers);
 
-      const notification = await request(
-        options,
-        function (error, response, body) {
-          console.log(response);
+      request.on("response", (headers, flags) => {
+        for (const name in headers) {
+          console.log(`${name}: ${headers[name]}`);
         }
-      );
+      });
+
+      request.setEncoding("utf8");
+
+      let data = "";
+      request.on("data", (chunk) => {
+        data += chunk;
+      });
+      request.write(JSON.stringify(body));
+      request.on("end", () => {
+        console.log(`\n${data}`);
+        client.close();
+      });
+      request.end();
 
       next({
         value: "OK",
